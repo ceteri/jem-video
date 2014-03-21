8ec00e42cdd4b63bf7a2b9fd48b736d3554a4c98
Paco Nathan <ceteri@gmail.com>
2014-03-21 16:04:57 -0700

    ch4 ex

---
+++ src/ch4_linalg.py
@@ -0,0 +1,14 @@
+import numpy.linalg as np
+
+a = 3
+b = 2
+x = [ 2, 3, 1, 0 ]
+y = [ 5, 1, 5, 0 ]
+
+print np.linalg.add(x, a)
+
+print np.linalg.dot(b, x)
+
+print np.linalg.add(x, y)
+
+print np.linalg.dot(x, y)

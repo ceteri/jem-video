12480cd5bbcffd4ca99965f61dc03a4aa1887456
Paco Nathan <ceteri@gmail.com>
2014-03-21 16:30:47 -0700

    ex

--- src/ch4_multipy.py
+++ src/ch4_multipy.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 A = np.array([1, 2, 3, 4]).reshape(2, 2)
-
 B = np.array([5, 6, 7, 8]).reshape(2, 2)
+x = [2, 3]
 
 print A
 print B
 
 print np.add(A, B)
+print np.dot(A, x)
